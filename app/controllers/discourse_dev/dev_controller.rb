caafdf30abb654556650741abea7f0592cb1e9f1
Evg <budo@narod.ru>
2017-10-07 18:43:54 +0300

    Create dev_controller.rb

---
+++ app/controllers/discourse_dev/dev_controller.rb
@@ -0,0 +1,12 @@
+module DiscourseDev
+	class DevController < ApplicationController
+
+		def my_page
+			render :json => { name: "Нас посетили за 24 часа... (в стадии разработки)" }
+      rescue StandardError => e
+        render_json_error e.message
+   
+		end
+
+	end
+end
