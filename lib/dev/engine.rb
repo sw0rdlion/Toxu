3f5127a4b99f557bc3cc88872e9d4d0e9bd89708
Evg <budo@narod.ru>
2017-10-10 00:49:46 +0300

    Update engine.rb

--- lib/dev/engine.rb
+++ lib/dev/engine.rb
@@ -9,3 +9,15 @@ module DiscourseDev
     end
   end
 end
+
+module DiscourseHots
+  class Engine < ::Rails::Engine
+    isolate_namespace DiscourseHots
+
+    config.after_initialize do
+		Discourse::Application.routes.append do
+			mount ::DiscourseHots::Engine, at: "/"
+		end
+    end
+  end
+end
