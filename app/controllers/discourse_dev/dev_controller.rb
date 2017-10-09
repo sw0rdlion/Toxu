7b0ead3d27b58222ec98e2a03a79db64f31a039a
Evg <budo@narod.ru>
2017-10-10 01:11:22 +0300

    Update dev_controller.rb

--- app/controllers/discourse_dev/dev_controller.rb
+++ app/controllers/discourse_dev/dev_controller.rb
@@ -2,7 +2,7 @@ module DiscourseDev
 	class DevController < ApplicationController
 
 		def my_page
-			render :json => { name: "Лучшие вопросы за последний месяц" }
+			render :json => { name: "Тут будут разные API данные и т.д. А еще одна новая, по ссылке ниже..." }
       rescue StandardError => e
         render_json_error e.message
    
