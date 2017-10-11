e880c0bff3a1bde5a5c5671a3b22c4b37f1e3ad9
Evg <budo@narod.ru>
2017-10-11 15:50:25 +0300

    Update hots_controller.rb

--- app/controllers/discourse_hots/hots_controller.rb
+++ app/controllers/discourse_hots/hots_controller.rb
@@ -2,7 +2,7 @@ module DiscourseHots
 	class HotsController < ApplicationController
 
 		def my_page
-			render :json => { name: "Лучшие вопросы за последний месяц" }
+			render :json => { name: "Лучшие вопросы за последний месяц (по просмотрам, лайкам, ответам, возвращениям...)" }
       rescue StandardError => e
         render_json_error e.message
    
