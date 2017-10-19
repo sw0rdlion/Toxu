c5565d9c480b6d5fd13f4f3357414aded1821c2c
Evg <budo@narod.ru>
2017-10-19 09:02:37 +0300

    Create toxu_controller.rb

---
+++ app/controllers/discourse_toxu/toxu_controller.rb
@@ -0,0 +1,12 @@
+module DiscourseToxu
+	class ToxuController < ApplicationController
+
+		def my_page
+			render :json => { name: "Что такое «Toxu»? И просто интересное о проекте... " }
+      rescue StandardError => e
+        render_json_error e.message
+   
+		end
+
+	end
+end
