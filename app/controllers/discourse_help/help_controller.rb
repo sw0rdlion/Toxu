c7a0a07f6e156a506bfc148541176301184a99af
Evg <budo@narod.ru>
2017-10-31 16:20:43 +0300

    Create help_controller.rb

---
+++ app/controllers/discourse_help/help_controller.rb
@@ -0,0 +1,12 @@
+module DiscourseHelp
+	class HelpController < ApplicationController
+
+		def my_page
+			render :json => { name: "Для чего нужен сайт Toxu.ru... " }
+      rescue StandardError => e
+        render_json_error e.message
+   
+		end
+
+	end
+end
