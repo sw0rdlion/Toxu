31cf116efcc23af38218fb3c96d1706426067e36
Evg <budo@narod.ru>
2017-11-27 20:58:02 +0300

    Update stats_controller.rb

--- app/controllers/discourse_stats/stats_controller.rb
+++ app/controllers/discourse_stats/stats_controller.rb
@@ -2,10 +2,11 @@ module DiscourseStats
 	class StatsController < ApplicationController
 
 		def my_page
-			render :json => { name: "Статистика аккаунта..." }
+			render :json => { name: "Для чего нужен сайт Toxu.ru... " }
       rescue StandardError => e
         render_json_error e.message
    
 		end
 
 	end
+end
