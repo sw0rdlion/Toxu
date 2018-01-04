f7c0b33af825e38ae3995e0d704b07ab76b306c5
Evg <budo@narod.ru>
2018-01-04 21:28:34 +0300

    Create features_controller.rb

---
+++ app/controllers/discourse_features/features_controller.rb
@@ -0,0 +1,12 @@
+module DiscourseFeatures
+	class FeaturesController < ApplicationController
+
+		def my_page
+			render :json => { name: "Возможности сайта Toxu.ru " }
+      rescue StandardError => e
+        render_json_error e.message
+   
+		end
+
+	end
+end
