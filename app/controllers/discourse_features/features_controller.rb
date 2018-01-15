19140a5760321eea30e3b5ed4fd5b5c1782cd372
Evg <budo@narod.ru>
2018-01-15 03:51:25 +0300

    Update features_controller.rb

--- app/controllers/discourse_features/features_controller.rb
+++ app/controllers/discourse_features/features_controller.rb
@@ -2,6 +2,10 @@ module DiscourseFeatures
 	class FeaturesController < ApplicationController
 
 		def my_page
+			
+			
+SiteSetting.site_description = "Возможности сайта Toxu. Разделы, теги, закладки, wiki сообщения, история правок..."
+			
 			render :json => { name: "Возможности сайта Toxu.ru " }
       rescue StandardError => e
         render_json_error e.message
@@ -12,4 +16,3 @@ module DiscourseFeatures
 end
 
 
-SiteSetting.site_description = "Возможности сайта Toxu. Разделы, теги, закладки, wiki сообщения, история правок..."
