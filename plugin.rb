dc54586379b81b41b25db2a0d0bf3372aaca4881
Evg <budo@narod.ru>
2018-02-05 13:27:41 +0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -6,6 +6,8 @@
 
 register_asset "stylesheets/sidebar.scss"
 
+enabled_site_setting :userbar_enabled
+
 Discourse::Application.routes.append do
   get '/dev' => 'discourse_dev/dev#my_page'
   get '/toxu' => 'discourse_toxu/toxu#my_page'
@@ -17,8 +19,17 @@ end
 
 load File.expand_path('../lib/dev/engine.rb', __FILE__)
 
+DiscoursePluginRegistry.serialized_current_user_fields << "see_userbar"
+DiscoursePluginRegistry.serialized_current_user_fields << "userbar_cat"
+DiscoursePluginRegistry.serialized_current_user_fields << "userbar_profile"
+
+
 after_initialize do
 
+User.register_custom_field_type('see_userbar', :boolean)
+User.register_custom_field_type('userbar_cat', :boolean)
+User.register_custom_field_type('userbar_profile', :boolean)  
+  
   require 'listable_topic_serializer'
   class ::ListableTopicSerializer
 
@@ -37,3 +48,5 @@ after_initialize do
     end
   end
 end
+
+register_asset "javascripts/discourse/templates/connectors/user-custom-preferences/userbar-preferences.hbs"
