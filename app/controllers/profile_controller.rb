8b3588fbab76957167c790b31b7c97c3d45802dc
Evg <budo@narod.ru>
2018-03-11 00:49:12 +0300

    Create profile_controller.rb

---
+++ app/controllers/profile_controller.rb
@@ -0,0 +1,17 @@
+class ProfileController < ApplicationController
+
+  layout false
+  skip_before_action :preload_json, :check_xhr, :redirect_to_login_if_required
+
+
+  def show
+    
+  @user = fetch_user_from_params(
+      { include_inactive: current_user.try(:staff?) || (current_user && SiteSetting.show_inactive_accounts) },
+      [{ user_profile: :card_image_badge }]
+  )
+    
+     path = SiteSetting.my_index ? :show : :no_show
+     render path, content_type: 'text/html'
+  end
+  end
