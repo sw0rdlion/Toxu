bc670dcd3e1e2374d83189d39d2b352b94e23ad4
Evg <budo@narod.ru>
2018-03-11 18:13:25 +0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -12,7 +12,7 @@ Discourse::Application.routes.append do
   get '/dev' => 'discourse_dev/dev#index'
   get '/toxu' => 'discourse_toxu/toxu#my_page'
   get '/features' => 'discourse_features/features#my_page'
-  get '/help' => 'discourse_help/help#my_page'
+  get '/help' => 'discourse_help/help#index'
   get '/stats' => 'discourse_stats/stats#my_page'
   get '/chess' => 'discourse_chess/chess#my_page'
   
