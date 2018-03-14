26d182a2e7dc0b38ce66920958ddc4ef4debbe47
Evg <budo@narod.ru>
2018-03-14 13:27:24 +0300

    Create profile_controller.rb

---
+++ app/controllers/discourse_profile/profile_controller.rb
@@ -0,0 +1,26 @@
+module DiscourseProfile
+	
+	class ProfileController < ApplicationController
+
+#  requires_login only: [:live_post_counts]
+
+  skip_before_action :check_xhr, only: [:index]
+
+  def index
+ #  return redirect_to path('/login') if SiteSetting.login_required? && current_user.nil?
+
+    @dev = About.new
+    respond_to do |format|
+      format.html do
+        render :index
+      end
+     format.json do
+       render_serialized(@dev, AboutSerializer)
+     end
+    end
+  end
+
+
+end
+	
+end
